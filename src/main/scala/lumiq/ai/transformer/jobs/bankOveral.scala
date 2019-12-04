5c53e508176086830339fa066edae752de55f681
rajat-lumiq <rajat.tanwar@lumiq.ai>
2019-12-04 17:52:22 +0530

    New

--- src/main/scala/lumiq/ai/transformer/jobs/bankOveral.scala
+++ src/main/scala/lumiq/ai/transformer/jobs/bankOveral.scala
@@ -13,18 +13,21 @@ object bankOveral {
 
     val wfunc = Window.orderBy(asc("RECON_SEQ_NO"))
 
-
     val rc_reconciled_txn = S3.ParqRead(spark,Constants.s3TransformedBucketName + Constants.indexedTablePath + Constants.RC_RECONCILED_TXN)
-      .select($"TERMINAL_ID".as("bank_acc_code"), $"RECON_STATUS", $"REMARKS".as("Knock_Off_Reference"),$"RECON_SEQ_NO")
+      .select($"TERMINAL_ID".as("bank_acc_code"), $"RECON_STATUS".as("Recon_Status"), $"REMARKS".as("Knock_Off_Reference"),$"RECON_SEQ_NO")
 
     val rc_leg_txn = S3.ParqRead(spark,Constants.s3TransformedBucketName + Constants.indexedTablePath + Constants.RC_LEG_TXN)
-      .select($"REF1", $"REF2", $"DATE_1".as("value_date"), $"DATE_2".as("transaction_date"), $"CHAR_1".as("cr_dr"),
-    $"AMT_1".as("amount"), $"REF3".as("description"), lit(null).cast("string").as("Remarks"),
+      .select($"REF1".as("Bank_Reference_One"), $"REF2".as("Bank_Reference_Two"), $"DATE_1".as("value_date")
+        , $"DATE_2".as("transaction_date"), $"CHAR_1".as("cr_dr"),
+    $"AMT_1".cast("double").cast("string").as("amount"), $"REF3".as("description"), lit(null).cast("string").as("Remarks"),
         lit(null).cast("string").as("ref_upload_entry"), lit(null).cast("string").as("recon_code"),$"RECON_SEQ_NO")
 
     val bankOveral_inter = rc_reconciled_txn.join(rc_leg_txn,Seq("RECON_SEQ_NO"),"inner").distinct()
 
-    val bankOveral = bankOveral_inter.withColumn("S.No.",(row_number.over(wfunc)).cast("string"))
+    val bankOveral = bankOveral_inter.withColumn("S_No",(row_number.over(wfunc)).cast("string"))
+      .select($"S_No",$"bank_acc_code",$"Recon_Status",$"recon_code",$"ref_upload_entry",$"Remarks",$"Knock_Off_Reference"
+        ,$"Bank_Reference_One",$"Bank_Reference_Two",$"bank_acc_code".as("Bank_Account_Number"),$"value_date"
+        ,$"transaction_date",$"cr_dr",$"amount",$"description")
 
     S3.ParqWrite(spark,bankOveral,Constants.s3TransformedBucketName + Constants.transformedPath + reportName + ".parquet")
 
